df5715d4ce042e0d6013ce53a89e67f819a7c810
Vincent Muchiri <vincentmuchiri@Vincents-MacBook-Air.local>
2022-02-11 12:09:04 +0300

    In the beninging...

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+	<div>
+		<header className="App-header">
+			<a href="/">omuzon</a>
+		</header>
+		<main>
+			List Products
+		</main>
+	</div>
+  );
+}
+
+export default App;
