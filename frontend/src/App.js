4611d4cbb810a60567dd2b694e1196d756d629f0
Vincent Muchiri <vincentmuchiri@Vincents-MacBook-Air.local>
2022-02-11 13:02:34 +0300

    display product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from "./utils/data";
+
 const App = () => {
   return (
 	<div>
@@ -5,7 +7,25 @@ const App = () => {
 			<a href="/">omuzon</a>
 		</header>
 		<main>
-			List Products
+			<h1>Featured Products</h1>
+			<div className="products">
+			{
+				data.products.map(product => (
+					<div className="product" key={product.slug}>
+						<a href={`/product/${product.slug}`}>
+							<img src={product.image} alt={product.name} />
+						</a>
+						<div className="product-info">
+							<a href={`/product/${product.slug}`}>
+								<p>{product.name}</p>
+							</a>
+							<p><strong>KES {product.price}</strong></p>
+							<button>Add to Cart</button>
+						</div>
+					</div>
+				))
+			}
+			</div>
 		</main>
 	</div>
   );
